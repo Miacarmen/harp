b36dfa0296acc74c071fa749a58b0c89019d09f1
Miacarmen <maitreyacarmen@gmail.com>
2022-11-04 23:27:04 -0700

    navbar created, base pages created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Components
+import Navbar from './components/Navigation/Navbar';
+import Footer from './components/Footer/Footer';
+// Pages
+import Home from './pages/Home';
+import Events from './pages/Events';
+import UserDash from './pages/UserDash';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <div className='wrapper'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/events' element={<Events />} />
+          <Route path='/dashboard' element={<UserDash />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} /> */}
+          {/* <Route path='/signup' element={<Signup />} />  */}
+          <Route path='*' element={<p>Page Not Found</p>} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  </>
   );
 }
 
 export default App;
+
---
+++ client/src/components/Navigation/Navbar.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Harp from '../../images/harp-wht.png';
+
+import './nav.styles.css';
+
+// TO-DO: if user not logged in, display login button
+// if they are logged in, display logout button
+
+const Navbar = () => {
+  // const [show, setShow] = useState(false);
+
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+
+  return (
+    <div className='nav'>
+      <div className='navbar bg-base-200 px-4'>
+        <div className='navbar-start'>
+          <img src={Harp} alt='logo' width='40px' height='40px' />
+        </div>
+        <div className='navbar-center'>
+          <div className='md:flex items-stretch space-x-3 font-medium '>
+            <NavLink to='/' className='navLink hover:text-secondary'>
+              Home
+            </NavLink>
+            <NavLink to='/events' className='navLink hover:text-secondary'>
+              Events
+            </NavLink>
+            <NavLink to='/dashboard' className='navLink hover:text-secondary'>
+              Dashboard
+            </NavLink>
+            <NavLink to='/contact' className='navLink hover:text-secondary'>
+              Contact
+            </NavLink>
+          </div>
+        </div>
+        <div className='navbar-end'>
+          <NavLink to='/login'>
+          <button className='btn-sm rounded-full bg-neutral border-none text-base-300 hover:bg-neutral-focus'>
+            Login
+          </button>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
