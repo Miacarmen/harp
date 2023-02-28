a0f1104d8dce1db3d8343833a3b94c6fb3867e0f
Miacarmen <maitreyacarmen@gmail.com>
2023-02-28 15:48:55 -0800

    server updated with mongoose

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+// const userRoutes = require('./routes/api/userRoutes');
+// const eventRoutes = require('./routes/api/eventRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 // Middleware for parsing json data
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -17,6 +22,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-app.listen(PORT, () => {
-  console.log(`App running on localhost: ${PORT}`);
-});
+// User Routes
+// app.use('/api/users', userRoutes);
+// app.use('/api/events', eventRoutes);
+
+// MongoDB Atlas connection string
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/harpDB';
+
+// Wrap Mongoose around local connection
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // Start Node + Express server on Port 4000
+    console.log('Mongoose connection successful!');
+    app.listen(PORT, () => {
+      console.log(`App running on localhost: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err, { message: 'Connection Failed' });
+  });
